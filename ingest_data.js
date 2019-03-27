03bc519ac45b55a83f4616d4ab91191fc914d5a7
Nero <nerochen25@berkeley.edu>
2019-03-26 21:10:29 -0700

    Add setTimeout method to log out bigData since it takes time to fetch data

--- ingest_data.js
+++ ingest_data.js
@@ -3,19 +3,23 @@ const fetch = require('node-fetch')
 
 
 const endPoint = 'https://api.ngs.nfl.com/league/schedule?season=2018&seasonType=REG';
+let bigData;
 
 async function getData() {
     const response = await fetch(endPoint);
     const data = await response.json();
-    dataObj = data;
 
     return data;
 }
 
-getData()
-    .then(response => {
-        response.forEach(data => {
-            console.log(data);
-        });
-    });
+async function assignData() {
+    bigData = await getData();
+}
+
+
+assignData();
+
+setTimeout(function(){
+    console.log(bigData)
+}, 1000);
 
